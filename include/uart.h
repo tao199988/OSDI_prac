5960e7a6933b0a608bce4accdf94834351114632
taom <windsoal@gmail.com>
2024-09-13 16:33:28 +0800

    uart & register mapping

---
+++ include/uart.h
@@ -0,0 +1,4 @@
+void uart_init(void);
+void uart_send(char c);
+char uart_recv(void);
+void uart_flush();
---
+++ src/uart.c
@@ -0,0 +1,11 @@
+#include "aux.h"
+#include "gpio.h"
+
+void uart_init(){
+    *AUX_ENABLES |= 1;
+    
+}
+
+
+
+
